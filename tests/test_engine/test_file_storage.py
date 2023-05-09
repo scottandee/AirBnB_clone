a579999f86b20862e3b5ad4d64417c80e9ff3d00
Tobi Tijani <tobi_tijani@yahoo.com>
2023-05-09 04:51:21 -0700

    Tests for Task 5, pycodestyle fixes

---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+"""This contains unittests for the FileStorage class"""
+
+
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+import json
+import os
+
+
+class TestClassAttributes(unittest.TestCase):
+    """This tests the attributes of the FileStorage Class"""
+
+    def test_file_path_is_string(self):
+        """This tests that __file_path class attribute
+        is a string"""
+        self.assertEqual(type(storage.__file_path), str)
+
+    def test_objects_is_dict(self):
+        """This tests that __objects class attribute
+        is a dictionary"""
+        self.assertEqual(type(storage.__objects), dict)
+
+
+class TestAll(unittest.TestCase):
+    """This tests the all() instance method of the FileStorage Class"""
+
+    def test_all_return_type(self):
+        """This tests that the all() method returns a dictionary"""
+        all_objs = storage.all()
+        self.assertEqual(type(all_objs), dict)
+
+    def test_all_return_instances(self):
+        """This tests that the all() method returns a dictionary
+        complete with all instances added"""
+        model1 = BaseModel()
+        model2 = BaseModel()
+        model3 = BaseModel()
+        key1 = f"{type(model1).__name__}.{model1.id}"
+        key2 = f"{type(model2).__name__}.{model2.id}"
+        key3 = f"{type(model3).__name__}.{model3.id}"
+        all_objs = storage.all()
+        self.assertTrue(key1 in all_objs)
+        self.assertTrue(key2 in all_objs)
+        self.assertTrue(key3 in all_objs)
+
+
+class TestNew(unittest.TestCase):
+    """This tests the new() instance method of the FileStorage Class"""
+
+    def test_new_instance_in_objects(self):
+        """This tests that the new() method properly adds an instance
+        to the __objects dictionary"""
+        model1 = BaseModel()
+        all_objs = storage.all()
+        all_objs_values = all_objs.values()
+        self.assertTrue(model1 in all_objs_values)
+
+    def test_key_format(self):
+        """This tests that the key created in the new()
+        method is the proper format"""
+        model1 = BaseModel()
+        key1 = f"{type(model1).__name__}.{model1.id}"
+        all_objs = storage.all()
+        test_key = {i for i in all_objs.items() if all_objs[i] == model1}
+        self.assertTrue(key1 in test_key)
+
+
+class TestSave(unittest.TestCase):
+    """This tests the save() instance method of the FileStorage Class"""
+
+    def test_json_file_exists(self):
+        """This tests that the save() method creates a JSON file"""
+        model = BaseModel()
+        model.name = "My_First_Model"
+        model.my_number = 89
+        model.save()
+        self.assertTrue(os.path.exists("file.json"))
+
+    def test_convert_to_json(self):
+        """This tests that the save() method properly converts the __objects
+        file to JSON"""
+        model = BaseModel()
+        model.name = "My_First_Model"
+        model.my_number = 89
+        model.save()
+        key = f"{type(model).__name__}.{model.id}"
+        with open("file.json", "r") as f:
+            string = f.read()
+        objs_json = json.loads(string)
+        self.assertTrue(key in objs_json)
+
+
+class TestReload(unittest.TestCase):
+    """This tests the reload() instance method of the FileStorage Class"""
+
+    def test_json_conversion_to_dict(self):
+        """This tests that the JSON file is properly converted back
+        to the __objects dictionary when reload() is called"""
+        model = BaseModel()
+        model.name = "My_First_Model"
+        model.my_number = 89
+        model.save()
+        all_objs = storage.all()
+        self.assertTrue(type(all_objs) is dict)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,52 +12,103 @@ class TestInstantiation(unittest.TestCase):
     """
 
     def test_id_is_unique(self):
+        """Tests that id attribute is unique for each instance
+of BaseModel()"""
         model1 = BaseModel()
         model2 = BaseModel()
         self.assertNotEqual(model2.id, model1.id)
+
     def test_created_is_instance_datetime(self):
+        """Tests that created_at attribute is a datetime object"""
         model = BaseModel()
         self.assertIsInstance(model.created_at, datetime)
+
     def test_updated_is_instance_datetime(self):
+        """Tests that updated_at attribute is a datetime object"""
         model = BaseModel()
-        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+
     def test_updated_is_same_with_created(self):
+        """Tests that created_at and updated_at are
+the same at instantiation"""
         model = BaseModel()
         diff = model.updated_at - model.created_at
         self.assertEqual(diff.seconds, 0)
         self.assertLess(diff.microseconds, 15)
 
+    def test_kwargs_not_added_to_dictionary(self):
+        """Tests that BaseModel instances created with kwargs are not
+        added to FileStorage.__objects"""
+        model_json = {'my_number': 89, 'name': 'My First Model',
+                      'updated_at': '2017-09-28T21:05:54.119572',
+                      'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579',
+                      'created_at': '2017-09-28T21:05:54.119427'}
+        new_model = BaseModel(**model_json)
+        key = f"{type(new_model).__name__}.{new_model.id}"
+        all_objs = storage.all()
+        self.assertFalse(key in all_objs)
+
+    def test_new_instance_added_to_dictionary(self):
+        """Tests that BaseModel instances created without kwargs
+        are added to FileStorage.__objects"""
+        model = BaseModel()
+        key = f"{type(model).__name__}.{model.id}"
+        all_objs = storage.all()
+        self.assertTrue(key in all_objs)
+
+
 class TestSaveMethod(unittest.TestCase):
     """This tests the save instance method"""
 
     def test_updated_at_different_from_created_at(self):
+        """Tests that updated_at attribute is different from created_at
+attribute after calling save() method"""
         model = BaseModel()
         model.save()
         self.assertNotEqual(model.created_at, model.updated_at)
 
     def test_updated_is_instance_datetime(self):
+        """Tests that new updated_at attribute is a datetime object"""
         model = BaseModel()
         model.save()
         self.assertIsInstance(model.created_at, datetime)
+
+    def test_save_contains_updated(self):
+        """This tests that the new updated_at time
+        is saved into __objects when save() is called"""
+        model = BaseModel()
+        model.name = "My_First_Model"
+        model.my_number = 89
+        model.save()
+        key = f"{type(model).__name__}.{model.id}"
+        all_objs = storage.all()
+        obj = all_objs[key]
+        self.assertEqual(model.updated_at, obj['updated_at'])
+
 
 class TestToDictMethod(unittest.TestCase):
     """This tests the to_dict() instance method"""
 
     def setUp(self):
+        """Setup for to_dict() Test Class"""
         self.model = BaseModel()
 
     def test_undercore_class_is_present(self):
+        """Tests that __class__ is a key in dictionary created by
+to_dict method"""
         dictionary = self.model.to_dict()
         self.assertEqual(dictionary['__class__'], type(self.model).__name__)
 
     def test_dates_in_isoformat(self):
+        """Tests that dates in to_dict() dictionary are in ISO format"""
         created_time = self.model.created_at
         updated_time = self.model.updated_at
         dictionary = self.model.to_dict()
         self.assertEqual(dictionary['created_at'], created_time.isoformat())
         self.assertEqual(dictionary['updated_at'], updated_time.isoformat())
 
-    def test_declared_intance_attributes(self):
+    def test_declared_instance_attributes(self):
+        """This tests the to_dict() instance method"""
         self.model.name = "Jane"
         self.model.number = 60
         dictionary = self.model.to_dict()
@@ -65,96 +116,110 @@ class TestToDictMethod(unittest.TestCase):
         self.assertEqual(dictionary['number'], self.model.number)
 
     def test_dates_equal_after_to_dict_call(self):
+        """This tests the to_dict() instance method"""
         created_time = self.model.created_at
         updated_time = self.model.updated_at
         dictionary = self.model.to_dict()
-        self.assertEqual(datetime.fromisoformat(dictionary['created_at']), created_time)
-        self.assertEqual(datetime.fromisoformat(dictionary['created_at']), created_time)
+        self.assertEqual(datetime.fromisoformat(dictionary['created_at']),
+                         created_time)
+        self.assertEqual(datetime.fromisoformat(dictionary['created_at']),
+                         created_time)
 
     def test_id_is_equal_after_dict_call(self):
+        """This tests the to_dict() instance method"""
         id_before_call = self.model.id
         dictionary = self.model.to_dict()
         self.assertEqual(id_before_call, dictionary['id'])
 
     def test_format_after_call(self):
+        """This tests the to_dict() instance method"""
         dic = self.model.to_dict()
         self.assertEqual(type(dic['created_at']), str)
         self.assertEqual(type(dic['updated_at']), str)
         self.assertEqual(type(dic['id']), str)
 
+
 class TestDisplayWhenPrinted(unittest.TestCase):
     """This contains the unuttests for the __str__ methods"""
-    
+
     def test_display(self):
+        """Tests the output of the __str__() method"""
         model = BaseModel()
         output = model.__str__()
         self.assertEqual(output, f"[{type(model).__name__}] ({model.id}) \
 {model.__dict__}")
 
+
 class TestInstantionWithKwargs(unittest.TestCase):
-    """This contains the unittests for the __init__ method 
+    """This contains the unittests for the __init__ method
     with the newly added kwargs parameter"""
 
     def test_kwargs_is_None(self):
+        """This tests when kwargs is None"""
         model = BaseModel(None)
         output = model.__str__()
         self.assertEqual(output, f"[{type(model).__name__}] ({model.id}) \
 {model.__dict__}")
 
     def test_kwargs_regular(self):
-        dic = {'__class__': 'BaseModel','updated_at':'2017-09-28T21:05:54.1195\
-72', 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28T2\
-1:05:54.119427'}
+        """This tests when kwargs is assigned"""
+        dic = {'__class__': 'BaseModel', 'updated_at': '2017-09-28T21:05:54.11\
+9572', 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28\
+T21:05:54.119427'}
         model = BaseModel(**dic)
         output = model.__str__()
         self.assertEqual(output, f"[{type(model).__name__}] ({model.id}) \
 {model.__dict__}")
 
     def test_kwargs_regular_with_args_present(self):
-        dic = {'__class__': 'BaseModel','updated_at':'2017-09-28T21:05:54.1195\
-72', 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28T2\
-1:05:54.119427'}
+        """This tests when kwargs is assigned with args also present"""
+        dic = {'__class__': 'BaseModel', 'updated_at': '2017-09-28T21:05:54.11\
+9572', 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28\
+T21:05:54.119427'}
         model = BaseModel(45, "string", **dic)
         output = model.__str__()
         self.assertEqual(output, f"[{type(model).__name__}] ({model.id}) \
 {model.__dict__}")
 
-
     def test_kwargs_with_declared_attrs(self):
+        """This tests when kwargs is assigned"""
         dic = {'my_number': 89, 'name': 'My First Model', '__class__': 'BaseMo\
-del','updated_at':'2017-09-28T21:05:54.119572', 'id': 'b6a6e15c-c67d-4312-9a7\
-5-9d084935e579', 'created_at': '2017-09-28T21:05:54.119427'}
+del', 'updated_at': '2017-09-28T21:05:54.119572', 'id': 'b6a6e15c-c67d-4312-9a\
+75-9d084935e579', 'created_at': '2017-09-28T21:05:54.119427'}
         model = BaseModel(**dic)
         output = model.__str__()
         self.assertEqual(output, f"[{type(model).__name__}] ({model.id}) \
 {model.__dict__}")
 
     def test_kwargs_with_declared_attrs_and_args(self):
+        """This tests when kwargs is assigned with args also present"""
         dic = {'my_number': 89, 'name': 'My First Model', '__class__': 'BaseMo\
-del','updated_at':'2017-09-28T21:05:54.119572', 'id': 'b6a6e15c-c67d-4312-9a7\
-5-9d084935e579', 'created_at': '2017-09-28T21:05:54.119427'}
+del', 'updated_at': '2017-09-28T21:05:54.119572', 'id': 'b6a6e15c-c67d-4312-9a\
+75-9d084935e579', 'created_at': '2017-09-28T21:05:54.119427'}
         model = BaseModel(45, "string", **dic)
         output = model.__str__()
         self.assertEqual(output, f"[{type(model).__name__}] ({model.id}) \
 {model.__dict__}")
 
     def test_create_not_same_object(self):
+        """Tests that instance created with kwargs is not the same
+object"""
         model1 = BaseModel()
         dic = model1.to_dict()
         model2 = BaseModel(**dic)
         self.assertFalse(model1 is model2)
 
     def test_kwargs_created_is_instance_datetime(self):
-        dic = {'__class__': 'BaseModel','updated_at':'2017-09-28T21:05:54.1195\
-72', 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28T2\
-1:05:54.119427'}
+        dic = {'__class__': 'BaseModel', 'updated_at': '2017-09-28T21:05:54.11\
+9572', 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28\
+T21:05:54.119427'}
         model = BaseModel(**dic)
         self.assertIsInstance(model.created_at, datetime)
 
     def test_underscore_class_is_not_instance_attr(self):
-        dic = {'__class__': 'BaseModel','updated_at':'2017-09-28T21:05:54.1195\
-72', 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28T2\
-1:05:54.119427'}
+        dic = {'__class__': 'BaseModel', 'updated_at': '2017-09-28T21:05:54.11\
+9572', 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28\
+T21:05:54.119427'}
         model = BaseModel()
         dic = model.__dict__
         with self.assertRaises(KeyError):
