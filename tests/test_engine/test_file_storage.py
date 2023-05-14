a2045d966cbe24bb18e270e1f11fe960ebab2d6a
Andee <andeeolayinkascott@gmail.com>
2023-05-14 18:48:58 +0000

    More file storage tests

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -37,9 +37,12 @@ class TestAll(unittest.TestCase):
     def test_all_return_instances(self):
         """This tests that the all() method returns a dictionary
         complete with all instances added"""
+        all_objs = storage.all()
+        len1 = len(all_objs)
         model1 = BaseModel()
         model2 = BaseModel()
         model3 = BaseModel()
+        len2 = len(all_objs)
         key1 = f"{type(model1).__name__}.{model1.id}"
         key2 = f"{type(model2).__name__}.{model2.id}"
         key3 = f"{type(model3).__name__}.{model3.id}"
@@ -47,6 +50,7 @@ class TestAll(unittest.TestCase):
         self.assertTrue(key1 in all_objs)
         self.assertTrue(key2 in all_objs)
         self.assertTrue(key3 in all_objs)
+        self.assertEqual(len1 + 3, len2)
 
 
 class TestNew(unittest.TestCase):
@@ -55,12 +59,15 @@ class TestNew(unittest.TestCase):
     def test_new_instance_in_objects(self):
         """This tests that the new() method properly adds an instance
         to the __objects dictionary"""
+        all_objs = storage.all()
+        len1 = len(all_objs)
         model1 = BaseModel()
         my_user = User()
-        all_objs = storage.all()
+        len2 = len(all_objs)
         all_objs_values = all_objs.values()
         self.assertTrue(model1 in all_objs_values)
         self.assertTrue(my_user in all_objs_values)
+        self.assertEqual(len2 - len1, 2)
 
     def test_key_format(self):
         """This tests that the key created in the new()
@@ -86,6 +93,16 @@ class TestSave(unittest.TestCase):
         model.my_number = 89
         model.save()
         self.assertTrue(os.path.exists("file.json"))
+
+    def test_type_in_which_json_file_is_stored(self):
+        """This tests that the json file is stored as a list"""
+        model = BaseModel()
+        model.save()
+        with open("file.json", "r") as f:
+            string = f.read()
+        self.assertTrue(type(string) is str)
+        objs_json = json.loads(string)
+        self.assertTrue(type(objs_json) is dict)
 
     def test_base_convert_to_json(self):
         """This tests that the save() method properly converts the __objects
@@ -115,6 +132,56 @@ class TestSave(unittest.TestCase):
         objs_json = json.loads(string)
         self.assertTrue(key in objs_json)
 
+    def test_city_convert_to_json(self):
+        """This tests that the save() method properly converts the __objects
+        file to JSON"""
+        my_city = City()
+        my_city.first_name = "Betty"
+        my_city.state_id = "2983u93"
+        my_city.name = "Comrade"
+        my_city.password = "root"
+        my_city.save()
+        key = f"{type(my_city).__name__}.{my_city.id}"
+        with open("file.json", "r") as f:
+            string = f.read()
+        objs_json = json.loads(string)
+        self.assertTrue(key in objs_json)
+
+    def test_state_convert_to_json(self):
+        """This tests that the save() method properly converts the __objects
+        file to JSON"""
+        my_state = State()
+        my_state.name = "Comrade"
+        my_state.save()
+        key = f"{type(my_state).__name__}.{my_state.id}"
+        with open("file.json", "r") as f:
+            string = f.read()
+        objs_json = json.loads(string)
+        self.assertTrue(key in objs_json)
+
+    def test_amenity_convert_to_json(self):
+        """This tests that the save() method properly converts the __objects
+        file to JSON"""
+        my_amenity = Amenity()
+        my_amenity.name = "Comrade"
+        my_amenity.save()
+        key = f"{type(my_amenity).__name__}.{my_amenity.id}"
+        with open("file.json", "r") as f:
+            string = f.read()
+        objs_json = json.loads(string)
+        self.assertTrue(key in objs_json)
+
+    def test_review_convert_to_json(self):
+        """This tests that the save() method properly converts the __objects
+        file to JSON"""
+        my_review = Review()
+        my_review.name = "Comrade"
+        my_review.save()
+        key = f"{type(my_review).__name__}.{my_review.id}"
+        with open("file.json", "r") as f:
+            string = f.read()
+        objs_json = json.loads(string)
+        self.assertTrue(key in objs_json)
 
 class TestReload(unittest.TestCase):
     """This tests the reload() instance method of the FileStorage Class"""
